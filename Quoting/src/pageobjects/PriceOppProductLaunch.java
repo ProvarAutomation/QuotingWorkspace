1029de1f6a8eb1440a3c9284bbd0503c06800551
Jason John <jaytj95@gmail.com>
2016-03-14 09:15:36 -0400

    Pricing PageObject

--- Quoting/src/pageobjects/PriceOppProductLaunch.java
+++ Quoting/src/pageobjects/PriceOppProductLaunch.java
@@ -14,14 +14,17 @@ import com.provar.core.testapi.annotations.*;
      )             
 public class PriceOppProductLaunch {
 
+	
 	@TextType()
-	@FindBy(xpath = "//*[@id=\"10:12;a\"]")
+	@FindBy(xpath = "//label/span[text()='Lot Size']/../../input")
 	public WebElement lotSize;
+	
 	@ChoiceListType()
-	@FindBy(xpath = "//*[@id=\"21:12;a\"]")
+	@FindBy(xpath = "//label/span[text()='Type:']/../../select")
 	public WebElement type;
-	@FindBy(id = "31:12;a")
+	
 	@TextType()
+	@FindBy(xpath = "//label/span[text()='Quantity:']/../../input")
 	public WebElement Quantity;
 	@ButtonType()
 	@FindByLabel(label = "Add")
@@ -29,38 +32,53 @@ public class PriceOppProductLaunch {
 	@ButtonType()
 	@FindByLabel(label = "Remove")
 	public WebElement remove;
+	
 	@ButtonType()
 	@FindByLabel(label = "Calculate")
 	public WebElement calculate;
+	
 	@ButtonType()
 	@FindByLabel(label = "Save Price")
 	public WebElement savePrice;
+	
 	@ButtonType()
 	@FindBy(xpath = "//div[5]/div[1]/input")
 	public WebElement addAdjust;
+	
 	@TextType()
 	@FindBy(xpath = "//div[5]/div[2]//input")
 	public WebElement discountPercent;
+	
 	@TextType()
-	@FindBy(id = "8:12;a")
+	@FindBy(xpath = "//label/span[text()='Lot Size']/../../input")
 	public WebElement LotSize;
-	@FindBy(id = "9:13;a")
+	
+	@FindBy(xpath = "//label/span[text()='DMA:']/../../select")
 	@ChoiceListType()
 	public WebElement Dma;
-	@FindBy(id = "25:13;a")
+	
+	@FindBy(xpath = "//label/span[text()='Alpha Zone:']/../../select")
 	@ChoiceListType()
 	public WebElement AlphaZone;
-	@FindBy(id = "44:13;a")
+	
+	@FindBy(xpath = "//label/span[text()='Make:']/../../select")
 	@ChoiceListType()
 	public WebElement Make;
+	
 	@ButtonType()
 	@FindByLabel(label = "Cancel")
 	public WebElement cancel;
+	
 	@ChoiceListType()
-	@FindBy(id = "8:13;a")
+	@FindBy(xpath = "//label/span[text()='Duration:']/../../input")
 	public WebElement Duration;
+	
 	@TextType()
 	@FindBy(id = "37:13;a")
 	public WebElement DMAList;
+	
+	@ChoiceListType()
+	@FindBy(xpath = "//label/span[text()='Type:']/../../select")
+	public WebElement Type;
 	
 }
